32b91eab0fd9bebbe861ce2f79f830c39b9bf811
Robert Gravina <robert.gravina@kii.com>
2014-09-26 12:42:22 +0900

    Initial Commit

---
+++ PragmaticTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by Robert Gravina on 9/26/14.
+//  Copyright (c) 2014 Robert Gravina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
