5b265edee12df1ff67b9f3b845afe61a07db3e8f
Robert Gravina <robert.gravina@gmail.com>
2014-09-29 13:52:51 +0900

    Send tweet using builtin SLComposeViewController.

--- PragmaticTweets/ViewController.swift
+++ PragmaticTweets/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleTweetButtonTapped(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            tweetVC.setInitialText("I just finished the first project in iOS 8 SDK Development. #pragsios8")
+            self.presentViewController(tweetVC, animated: true, completion: nil)
+        } else {
+            println("Can't send tweet")
+        }
+    }
 
 }
 
