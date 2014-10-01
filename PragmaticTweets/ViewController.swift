1f9c81f6715514e86f53a0003ad3d604ed78ff93
Robert Gravina <robert.gravina@gmail.com>
2014-10-01 23:06:33 +0900

    Reduced tab spacing to two spaces

--- PragmaticTweets/ViewController.swift
+++ PragmaticTweets/ViewController.swift
@@ -10,46 +10,36 @@ import UIKit
 import Social
 
 public class ViewController: UITableViewController {
+  override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 5;
+  }
 
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Section \(section)"
+  }
 
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override public func tableView(_tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return section + 1;
+  }
+  
+  override public func tableView (_tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+    cell.textLabel?.text = "Row \(indexPath.row)"
+    return cell 
+  }
 
-    override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 5;
+  @IBAction func handleTweetButtonTapped(sender: UIButton) {
+    if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+      let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+      let message = NSBundle.mainBundle().localizedStringForKey(
+          "I just finished the first project in iOS 8 SDK Development. #pragsios8",
+          value: "",
+          table: nil)
+      tweetVC.setInitialText(message)
+      self.presentViewController(tweetVC, animated: true, completion: nil)
+    } else {
+      println("Can't send tweet")
     }
-
-    override public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
-    }
-
-    override public func tableView(_tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section + 1;
-    }
-    
-    override public func tableView (_tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-        cell.textLabel?.text = "Row \(indexPath.row)"
-        return cell 
-    }
-
-    @IBAction func handleTweetButtonTapped(sender: UIButton) {
-        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
-            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
-            let message = NSBundle.mainBundle().localizedStringForKey(
-                "I just finished the first project in iOS 8 SDK Development. #pragsios8",
-                value: "",
-                table: nil)
-            tweetVC.setInitialText(message)
-            self.presentViewController(tweetVC, animated: true, completion: nil)
-        } else {
-            println("Can't send tweet")
-        }
-    }
+  }
 }
 
