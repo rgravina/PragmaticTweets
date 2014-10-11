439a647eac06a4d85e3413ce838a35e9e3f26680
Robert Gravina <robert.gravina@gmail.com>
2014-10-11 21:30:57 +1100

    Update indentation.

--- PragmaticTweets/UserDetailViewController.swift
+++ PragmaticTweets/UserDetailViewController.swift
@@ -9,27 +9,33 @@
 import UIKit
 
 class UserDetailViewController: UIViewController {
+  @IBOutlet weak var userImageView: UIImageView!
+  @IBOutlet weak var userRealNameLabel: UILabel!
+  @IBOutlet weak var userScreenNameLabel: UILabel!
+  @IBOutlet weak var userLocationLabel: UILabel!
+  @IBOutlet weak var userDescriptionLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var screenName : String?
 
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // Do any additional setup after loading the view.
+  }
 
-    /*
-    // MARK: - Navigation
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
