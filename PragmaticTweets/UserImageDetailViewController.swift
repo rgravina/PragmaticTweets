e3d908e09eb5157ea3c9c161272bdbea9cb4cf2d
Robert Gravina <robert.gravina@gmail.com>
2014-10-30 14:00:24 +0900

    Pan image around.

--- PragmaticTweets/UserImageDetailViewController.swift
+++ PragmaticTweets/UserImageDetailViewController.swift
@@ -3,12 +3,24 @@ import UIKit
 class UserImageDetailViewController: UIViewController {
   @IBOutlet weak var userImageView: UIImageView!
   
-  var userImageURL : NSURL?
-  
+  var userImageURL: NSURL?
+  var preGestureTransform: CGAffineTransform?
+
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
     if self.userImageURL != nil {
       self.userImageView.image = UIImage(data: NSData(contentsOfURL: self.userImageURL!)!)
+    }
+  }
+
+  @IBAction func handlePanGesture(sender: UIPanGestureRecognizer) {
+    if sender.state == .Began {
+      self.preGestureTransform = self.userImageView.transform
+    }
+    if sender.state == .Began || sender.state == .Changed {
+      let translation = sender.translationInView(self.userImageView)
+      let translatedTransform = CGAffineTransformTranslate(self.preGestureTransform!, translation.x, translation.y)
+      self.userImageView.transform = translatedTransform
     }
   }
 }
